0b20f7dd0433d9dc86d3177171ec62ce1c3f7d59
Klaas Reineke <klaas.reineke@outlook.com>
2022-01-25 10:06:27 +0100

    Removed tables test for not checked in copyrighted tables

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/test/tables/'],
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/test/tsconfig.test.json',
